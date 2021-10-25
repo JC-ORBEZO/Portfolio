c9c37fb0a10d2099f1cb4be478059efc9dd3ee29
Jose <Jose.orbezo@alumnos.frgp.utn.edu.ar>
2021-10-25 10:41:52 -0300

    reemplace display none por visibility

--- index.js
+++ index.js
@@ -1,15 +1,25 @@
-const ocultar=(boton,ocultar,mensaje)=>{
-    //  SET TIME
-    setTimeout(function(){
-    const botone=document.getElementById(boton);
-    const rata=document.getElementById(ocultar);
+const ocultar = (boton, ocultar, mensaje) => {
+  //  SET TIME
+  setTimeout(function () {
+    const botone = document.getElementById(boton);
+    const rata = document.getElementById(ocultar);
     //rata.style.display="none";
-    if(botone.textContent=="➖"){
-        botone.textContent=`➕ ${mensaje}`
-        rata.style.display="none";        
-    }else{
-        botone.textContent="➖";
-        rata.style.display="block";
+    if (botone.textContent == "➖") {
+      botone.textContent = `➕ ${mensaje}`;
+      //rata.style.display = "none";
+      rata.style.visibility = "hidden";
+    } else {
+      botone.textContent = "➖";
+      //rata.style.display = "block";
+      rata.style.visibility = "visible";
     }
-    }, 200);
+  }, 200);
+};
+/*
+const Ubicaina=()=>{
+    const proba=document.getElementById("ubicacion");
+    console.log(proba);
+    console.log(document.getElementById("ciudad").textContent);
 }
+
+Ubicaina();*/
